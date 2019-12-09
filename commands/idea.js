ea390cc0ef4fd1c50ad9f8af527570ef30d1988e
Filda4515 <48486481+Filda4515@users.noreply.github.com>
2019-12-09 22:13:04 +0100

    Update 1.0.3
    .idea info <text> command

--- commands/idea.js
+++ commands/idea.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
 	
 	message.delete()
 	if(!args[0]) return message.channel.send("Nenapsal jsi žádný nápad.").then(m => m.delete(3000));
-	if(message.author.id === '356168492942229506') {
+	if(message.author.id === "356168492942229506") {
 		
 		mention = message.mentions.members.first();
 		
@@ -20,14 +20,31 @@ module.exports.run = async (bot, message, args) => {
 				.setAuthor("Filda4515 Bot IDEA", message.guild.iconURL)
 				.setThumbnail(bot.user.displayAvatarURL)
 				.setTimestamp()
-				.setDescription(message.content.slice(prefix.length+3+args[0].length+args[1].length+args[2].length))
+				.setDescription(message.content.slice(prefix.length+4+1+args[0].length+1+args[1].length+1))
 				.addField("Ticket info:", "Tvůj nápad byl schválen a bude brzy implementován.\nPokud máte více nápadů, nebojte se nám je sdělit přes .idea <nápad>\nDěkujeme za váš čas.")
 				.setFooter("Filda4515 Bot", bot.user.displayAvatarURL)
 			mention.sendMessage (Aembed);
 			let Bembed = new Discord.RichEmbed()
 				.setColor("#003EFF")
-				.setAuthor(message.content.slice(prefix.length+3+args[0].length+args[1].length+args[2].length))
+				.setAuthor(message.content.slice(prefix.length+4+1+args[0].length+1+args[1].length+1))
 				.setDescription("Nápad byl schválen.")
+				.setFooter("Filda4515 Bot", bot.user.displayAvatarURL)
+			return channel.send(Bembed);
+		} else if(args[0] === "info") {
+			let Aembed = new Discord.RichEmbed()
+				.setColor("#003EFF")
+				.setAuthor("Filda4515 Bot IDEA", message.guild.iconURL)
+				.setThumbnail(bot.user.displayAvatarURL)
+				.setTimestamp()
+				.setDescription("Tvůj ticket zatím nemohl být přijat, ani odmítnut z těchto důvodů:")
+				.addField("Ticket info:", message.content.slice(prefix.length+4+1+args[0].length+1+args[1].length+1))
+				.addField("Pokud je třeba zdělit podrobnosti použijte:", `${prefix}idea info <text>`)
+				.setFooter("Filda4515 Bot", bot.user.displayAvatarURL)
+			mention.sendMessage (Aembed);
+			let Bembed = new Discord.RichEmbed()
+				.setColor("#003EFF")
+				.setAuthor(message.content.slice(prefix.length+4+1+args[0].length+1+args[1].length+1))
+				.setDescription("Informace o nápadu byly poslány")
 				.setFooter("Filda4515 Bot", bot.user.displayAvatarURL)
 			return channel.send(Bembed);
 		} else if(args[0] === "deny") {
@@ -36,19 +53,27 @@ module.exports.run = async (bot, message, args) => {
 				.setAuthor("Filda4515 Bot IDEA", message.guild.iconURL)
 				.setThumbnail(bot.user.displayAvatarURL)
 				.setTimestamp()
-				.setDescription(message.content.slice(prefix.length+2+args[0].length+args[1].length+args[2].length))
+				.setDescription(message.content.slice(prefix.length+4+1+args[0].length+1+args[1].length+1))
 				.addField("Ticket info:", "Tvůj nápad byl bohužel zamítnut.\nPokud si myslíš, že by měl být povolen, vytvoř prosím znovu ticket přes .idea <nápad> a popiš podrobnosti proč by měl být tvůj návrh přijat.")
 				.setFooter("Filda4515 Bot", bot.user.displayAvatarURL)
 			mention.sendMessage (Aembed);
 			let Bembed = new Discord.RichEmbed()
 				.setColor("#003EFF")
-				.setAuthor(message.content.slice(prefix.length+3+args[0].length+args[1].length+args[2].length))
+				.setAuthor(message.content.slice(prefix.length+4+1+args[0].length+1+args[1].length+1))
 				.setDescription("Nápad byl zamítnut.")
 				.setFooter("Filda4515 Bot", bot.user.displayAvatarURL)
 			return channel.send(Bembed);
 		}
 	}
 	
+	if(args[0] === "info" && message.author.id != "356168492942229506") {
+		const DMembed = new Discord.RichEmbed()
+			.setColor("#003EFF")
+			.setAuthor(message.author.tag, message.author.displayAvatarURL)
+			.setDescription("Informační ticket:")
+			.addField("Informace:", message.content.slice(prefix.length+4+1+args[0].length+1+args[1].length+1))
+		return bot.users.get("356168492942229506").sendMessage (DMembed);
+	}
 	//guild.createChannel(`${message.author.username}-${message.author.discriminator}`, "text", {"READ_MESSAGES": false}).then(channel => {channel.setParent(category)});
 	//channel = message.guild.channels.find(channel => channel.name === `${message.author.username}-${message.author.discriminator}`);
 	
@@ -56,8 +81,8 @@ module.exports.run = async (bot, message, args) => {
 		.setColor("#003EFF")
 		.setAuthor(message.author.tag, message.author.displayAvatarURL)
 		.setDescription(message.content.slice(prefix.length+5))
-		.setFooter("Návrh pro bota || .idea accept @user <napad> / .idea deny @user <napad>")
-	channel.send(Embed);
+		.setFooter("Návrh pro bota || .idea accept @user <napad> / .idea info @user <info> / .idea deny @user <napad>")
+	return channel.send(Embed);
 }
 
 module.exports.config = {
