82f518c7017c6a306e75ec7ccd4e3d469f2d82ff
Filda4515 <filip4515@gmail.com>
2022-01-02 21:00:21 +0100

    patch + vc test

--- commands/admin.js
+++ commands/admin.js
@@ -7,7 +7,11 @@ module.exports.run = async (bot, message, args) => {
 	if(message.author.id != '356168492942229506') return message.channel.send("Nemáš oprávnění");
 
 	let id = args[0].slice(2, args[0].length-1);
-	if(args[0].slice(1, 2) == "#") return bot.channels.get(id).send(message.content.slice(prefix.length+5+1+args[0].length+1));
+	if(args[0].slice(1, 2) == "#") 
+    {
+        let channel = await bot.channels.fetch(id);
+        return channel.send(message.content.slice(prefix.length+5+1+args[0].length+1));
+    }
 	if(args[0].slice(1, 2) == "@") return message.mentions.members.first().send(message.content.slice(prefix.length+5+1+args[0].length+1));
 }
 
--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const bot = new Discord.Client({ intents });
 bot.on("ready", async () => {
 	console.log(`${bot.user.username} is online!`);
 	bot.user.setActivity("gachiBASS", {type: "LISTENING"});
-})
+    
+    
+    //const clientId = "649630182042370069";
+
+    //const guildId = "510138523421114368";
+    //const guild = bot.guilds.cache.get(guildId);
+});
 
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
@@ -23,13 +29,13 @@ fs.readdir("./commands/", (err, files) => {
 		return console.log("[LOGS] Couldn't find Commands!");
 	}
 	
-	jsfile.forEach((f, i) => {
-		let pull = require(`./commands/${f}`);
-		bot.commands.set(pull.config.name, pull);
-		pull.config.aliases.forEach(alias => {
-			bot.aliases.set(alias, pull.config.name)
+	for (const file of jsfile) {
+		let command = require(`./commands/${file}`);
+		bot.commands.set(command.config.name, command);
+		command.config.aliases.forEach(alias => {
+			bot.aliases.set(alias, command.config.name)
 		});
-	});
+	};
 });
 
 bot.on("messageCreate", async message => {
@@ -43,20 +49,28 @@ bot.on("messageCreate", async message => {
 	if(!message.content.startsWith(prefix)) return;
 	let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
 	if(commandfile) commandfile.run(bot,message,args)
-})
+});
 
-bot.on("voiceStateUpdate", (oldMember, newMember)=> { 
-  let oldVoice = oldMember.voiceChannelID; 
-  let newVoice = newMember.voiceChannelID; 
-  if (oldVoice != newVoice) {
-    if (oldVoice == null) {
-      console.log("User joined!" + oldMember + " " + newMember);
-    } else if (newVoice == null) {
-      console.log("User left!" + oldMember+ " " + newMember);
-    } else {
-      console.log("User switched channels!" + oldMember + " " + newMember);
-    }
-  }
+bot.on("voiceStateUpdate", async (oldState, newState) => {
+    let oldVoiceId = oldState.channelId;
+    let oldVoice = oldState.channel;    
+    let newVoiceId = newState.channelId;
+    let newVoice = newState.channel;
+    let user = await bot.users.fetch(oldState.id);
+
+    if (oldVoiceId != newVoiceId) {
+        if (oldVoiceId == null) {
+          console.log(user.username + " joined to " + newVoice.name);
+          if (newVoice.name == "forever_alone") console.log(user.username + " je v píči :(");
+        } else if (newVoiceId == null) {
+          console.log(user.username + " left from " + oldVoice.name);
+          if (oldVoice.name == "forever_alone") console.log(user.username + " už není v píči :)");
+        } else {
+          console.log(user.username + " switched channels from " + oldVoice.name + " to " + newVoice.name);
+          if (newVoice.name == "forever_alone") console.log(user.username + " je v píči :(");
+          if (oldVoice.name == "forever_alone") console.log(user.username + " už není v píči :)");
+        }
+    } 
 });
 
 bot.login(process.env.secrettoken);
